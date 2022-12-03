c5251662d87affb7d38a3fe7a38bf5be135ca13c
lorycris99 <65489866+lorycris99@users.noreply.github.com>
2022-12-03 15:57:37 +0100

    Made the block version of BWT parallel

--- src/compressione.py
+++ src/compressione.py
@@ -4,71 +4,93 @@ import Rle.rle as rle
 from dahuffman import HuffmanCodec 
 import time
 import pickle
+import multiprocessing
 
-inputFile = open("TestFiles/Input/PROVA.txt", "r")
-listInput = inputFile.readlines()
-stringInput = ""
-for val in listInput:
-    stringInput += val
+def block_bwt(input, index, return_dict):
+    outputBWT = bwt.bwt_from_suffix(input)
+    return_dict[index] = outputBWT
 
-dictionary = set(stringInput)
-dictionary.add("\003")
+if __name__ == "__main__":
+    inputFile = open("TestFiles/Input/PROVA.txt", "r")
+    listInput = inputFile.readlines()
+    stringInput = ""
+    for val in listInput:
+        stringInput += val
 
-#BWT
-print("Starting BWT...")
-bwtStartTime = time.time()
+    dictionary = set(stringInput)
+    dictionary.add("\003")
 
-# Codice per eseguire la BWT a blocchi
+    #BWT
+    print("Starting BWT...")
+    bwtStartTime = time.time()
 
+    # Codice per eseguire la BWT a blocchi
 
-block_lenght = 1024*300
-using_blocks = True
-outputBWT = ""
-if using_blocks and len(stringInput) > block_lenght:
-    print("Block mode")
-    for i in range(0, len(stringInput),block_lenght):
-        input_block = stringInput[i:i+block_lenght] + "\003" # Add EOF
-        outputBWT += bwt.bwt_from_suffix(input_block)
-else:
-    print("Full file mode")
-    stringInput += "\003" # Add EOF
-    outputBWT = bwt.bwt_from_suffix(stringInput)
+    block_lenght = 1024*300
+    using_blocks = True
+    outputBWT = ""
 
-bwtElapsedTime = time.time() - bwtStartTime
-print(str(bwtElapsedTime) + "  -> elapsed time of BWT")
-fileOutputBWT = open("TestFiles/Output/outputBWT.txt", "w+")
-fileOutputBWT.write(outputBWT)
-#salvo il dizionario della BWT
-fileOutputDictBWT = open("TestFiles/Output/outputDictBWT.txt", "w+")
-dictStr = ""
-for element in set(outputBWT):
-    dictStr += element
-fileOutputDictBWT.write(dictStr)
+    if using_blocks and len(stringInput) > block_lenght:
+        print("Block mode")
+        # Creo il dizionario condiviso
+        manager = multiprocessing.Manager()
+        return_dict = manager.dict()
+        # Creo l'indice e i processi
+        j=0
+        process_list = []
+        for i in range(0, len(stringInput),block_lenght):
+            input_block = stringInput[i:i+block_lenght] + "\003" # Add EOF
+            p = multiprocessing.Process(target=block_bwt, args=(input_block, j, return_dict))
+            j+=1
+            process_list.append(p)
+            p.start()
 
-#MTF
-print("Starting MTF...")
-mtf_start_time = time.time()
-#print(sorted(dictionary))
-outputMTF = mtf.encode(plain_text=outputBWT, dictionary=sorted(dictionary)) 
-mtf_elapsed_time = time.time() - mtf_start_time
-print(str(mtf_elapsed_time) + "  -> elapsed time of MTF")
-fileOutputMTF = open("TestFiles/Output/outputMTF.txt", "w+")
-fileOutputMTF.write(str(outputMTF).replace(" ", ""))
+        for p in process_list:
+            p.join()
 
-#RLE
-print("Starting RLE")
-rleModule = rle.Rle()
-rle_start_time = time.time()
-outputRLE = rle.Rle.rle_encode(rleModule, data=list(map(str, outputMTF))) # trasformo la lista di interi in lista di stringhe
-rle_elapsed_time = time.time() - rle_start_time
-print(str(rle_elapsed_time) + "  -> elapsed time of RLE")
-fileOutputRLE = open("TestFiles/Output/outputRLE.txt", "w+")
-fileOutputRLE.write(str(outputRLE))
+        for i in range(0, j):
+            outputBWT += return_dict[i]
 
-#PC
-codec = HuffmanCodec.from_data(outputRLE)
-encoded = codec.encode(outputRLE)
-fileOutputPC = open("TestFiles/Output/outputPC.txt", "wb")
-fileOutputPCCodec = open("TestFiles/Output/outputPCCodec.txt", "wb")
-pickle.dump(encoded, fileOutputPC)
-pickle.dump(codec, fileOutputPCCodec)
+    else:
+        print("Full file mode")
+        stringInput += "\003" # Add EOF
+        outputBWT = bwt.bwt_from_suffix(stringInput)
+
+    bwtElapsedTime = time.time() - bwtStartTime
+    print(str(bwtElapsedTime) + "  -> elapsed time of BWT")
+    fileOutputBWT = open("TestFiles/Output/outputBWT.txt", "w+")
+    fileOutputBWT.write(outputBWT)
+    #salvo il dizionario della BWT
+    fileOutputDictBWT = open("TestFiles/Output/outputDictBWT.txt", "w+")
+    dictStr = ""
+    for element in set(outputBWT):
+        dictStr += element
+    fileOutputDictBWT.write(dictStr)
+
+    #MTF
+    print("Starting MTF...")
+    mtf_start_time = time.time()
+    #print(sorted(dictionary))
+    outputMTF = mtf.encode(plain_text=outputBWT, dictionary=sorted(dictionary)) 
+    mtf_elapsed_time = time.time() - mtf_start_time
+    print(str(mtf_elapsed_time) + "  -> elapsed time of MTF")
+    fileOutputMTF = open("TestFiles/Output/outputMTF.txt", "w+")
+    fileOutputMTF.write(str(outputMTF).replace(" ", ""))
+
+    #RLE
+    print("Starting RLE")
+    rleModule = rle.Rle()
+    rle_start_time = time.time()
+    outputRLE = rle.Rle.rle_encode(rleModule, data=list(map(str, outputMTF))) # trasformo la lista di interi in lista di stringhe
+    rle_elapsed_time = time.time() - rle_start_time
+    print(str(rle_elapsed_time) + "  -> elapsed time of RLE")
+    fileOutputRLE = open("TestFiles/Output/outputRLE.txt", "w+")
+    fileOutputRLE.write(str(outputRLE))
+
+    #PC
+    codec = HuffmanCodec.from_data(outputRLE)
+    encoded = codec.encode(outputRLE)
+    fileOutputPC = open("TestFiles/Output/outputPC.txt", "wb")
+    fileOutputPCCodec = open("TestFiles/Output/outputPCCodec.txt", "wb")
+    pickle.dump(encoded, fileOutputPC)
+    pickle.dump(codec, fileOutputPCCodec)
--- src/decompressione.py
+++ src/decompressione.py
@@ -4,62 +4,86 @@ import Rle.rle as rle
 import time
 import pickle
 from dahuffman import HuffmanCodec
+import multiprocessing
 
-start = time.time()
-# leggo il dizionario salvato dalla bwt in fase di compressione
-dictionaryFile = open("TestFiles/Output/outputDictBWT.txt")
-dictionaryLines = dictionaryFile.readlines()
-dictionaryStr = ""
-for string in dictionaryLines:
-    dictionaryStr += string
+def block_bwt(input, index, return_dict):
+    output = bwt.ibwt_from_suffix(input)
+    return_dict[index] = output
 
 
-# IPC
-encodedFile = open("TestFiles/Output/outputPC.txt", "rb")
-codecFile = open("TestFiles/Output/outputPCCodec.txt", "rb")
-encoded = pickle.load(encodedFile)
-codec = pickle.load(codecFile)
-outputPC = codec.decode(encoded)
+if __name__ == "__main__":
 
-# IRLE
-'''rleFile = open("TestFiles/Output/outputRLE.txt", "r")
-rleLines = rleFile.readlines()
-rleString = ""
-for val in rleLines:
-    rleString += val'''
-rleModule = rle.Rle()
-rleDecodedString = rle.Rle.rle_decode(rleModule, data=outputPC)
-#print(rleDecodedString)
+    start = time.time()
+    # leggo il dizionario salvato dalla bwt in fase di compressione
+    dictionaryFile = open("TestFiles/Output/outputDictBWT.txt")
+    dictionaryLines = dictionaryFile.readlines()
+    dictionaryStr = ""
+    for string in dictionaryLines:
+        dictionaryStr += string
 
-# IMTF
-mtfList = rleDecodedString.split(",")
-res = []
-for i in mtfList:
-    res.append(int(i))
-mtfDecodedString = mtf.decode(res, dictionary=sorted(dictionaryStr))
-#print("-----MTF: " + mtfDecodedString)
 
-# IBWT
+    # IPC
+    encodedFile = open("TestFiles/Output/outputPC.txt", "rb")
+    codecFile = open("TestFiles/Output/outputPCCodec.txt", "rb")
+    encoded = pickle.load(encodedFile)
+    codec = pickle.load(codecFile)
+    outputPC = codec.decode(encoded)
 
-# Dividi in blocchi mtfDecodedString
-block_lenght = 1024*300 +1 # Deve essere la stessa usata in compressione +1 per l'EOF
-using_blocks = True
-bwtDecodedString = []
-if using_blocks and len(mtfDecodedString) > block_lenght:
-    print("Block mode")
-    for i in range(0, len(mtfDecodedString),block_lenght):
-        input_block = mtfDecodedString[i:i+block_lenght]
-        bwtDecodedString.extend(bwt.ibwt_from_suffix(input_block)) 
+    # IRLE
+    '''rleFile = open("TestFiles/Output/outputRLE.txt", "r")
+    rleLines = rleFile.readlines()
+    rleString = ""
+    for val in rleLines:
+        rleString += val'''
+    rleModule = rle.Rle()
+    rleDecodedString = rle.Rle.rle_decode(rleModule, data=outputPC)
+    #print(rleDecodedString)
 
-else:
-    print("Full file mode")
-    bwtDecodedString = bwt.ibwt_from_suffix(mtfDecodedString)
+    # IMTF
+    mtfList = rleDecodedString.split(",")
+    res = []
+    for i in mtfList:
+        res.append(int(i))
+    mtfDecodedString = mtf.decode(res, dictionary=sorted(dictionaryStr))
+    #print("-----MTF: " + mtfDecodedString)
 
-print(bwtDecodedString)
-outputBWTFile = open("TestFiles/Output/decompresso.txt", "w+")
-outputBWTString = ""
-for i in range(0, len(bwtDecodedString)):
-    outputBWTString += bwtDecodedString[i]
-outputBWTFile.write(str(outputBWTString))
+    # IBWT
 
-print("elapsed time: " + str(time.time() - start))
+    # Dividi in blocchi mtfDecodedString
+    block_lenght = 1024*300 +1 # Deve essere la stessa usata in compressione +1 per l'EOF
+    using_blocks = True
+    bwtDecodedString = []   
+
+    if using_blocks and len(mtfDecodedString) > block_lenght:
+        print("Block mode")
+        # Creo la variabile condivisa
+        manager = multiprocessing.Manager()
+        return_dict = manager.dict()
+
+        # Divido l'input e creo i processi
+        j = 0
+        processList = []
+        for i in range(0, len(mtfDecodedString),block_lenght):
+            input_block = mtfDecodedString[i:i+block_lenght]
+            p = multiprocessing.Process(target=block_bwt, args=(input_block, j, return_dict))
+            j+=1
+            processList.append(p)
+            p.start()
+        
+        for p in processList:
+            p.join()
+
+        for i in range(0,j):
+            bwtDecodedString.extend(return_dict[i])
+    else:
+        print("Full file mode")
+        bwtDecodedString = bwt.ibwt_from_suffix(mtfDecodedString)
+
+    #print(bwtDecodedString)
+    outputBWTFile = open("TestFiles/Output/decompresso.txt", "w+")
+    outputBWTString = ""
+    for i in range(0, len(bwtDecodedString)):
+        outputBWTString += bwtDecodedString[i]
+    outputBWTFile.write(str(outputBWTString))
+
+    print("elapsed time: " + str(time.time() - start))
